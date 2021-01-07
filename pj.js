d8537e289f9070536606910eeae655f73fb16651
CCSU-Fan <52030019+CCSU-Fan@users.noreply.github.com>
2021-01-07 13:50:00 +0800

    Update pj.js

--- pj.js
+++ pj.js
@@ -19,24 +19,6 @@ var Lists = document.getElementsByTagName("input");//获取所有input组件
 
 //document.getElementById("tj").onclick();//触发提交函数
 
-var Lists = document.getElementsByTagName("input");//获取所有input组件
-	for (i=0; i<Lists.length; i++) {
-		
-		var name = Lists[i].getAttribute("name");//不同的name
-		var radioxh = Lists[i].getAttribute("radioxh");//0-非常满意 1-满意
-		if( radioxh=="0")
-		{
-		      Lists[i].checked = true;//选择
-		}
-		
-		if(name=="radio3" && radioxh=="1")//不能全部选择同一选项，改选一个
-		{
-		       Lists[i].checked = true;
-		}
-
-		
-		
-	}
 	////保存评教信息 function saveData(obj)
 	var elements=window.Form1.elements;
 	var count=0;
